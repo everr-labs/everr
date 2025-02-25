90844294178f3fd27aa5bd548e20a0e302cbbdd1
Giordano Ricci <me@giordanoricci.com>
2025-02-25 22:03:25 +0000

    update deps

--- packages/app/app/lib/auth.ts
+++ packages/app/app/lib/auth.ts
@@ -5,7 +5,7 @@ import { organization } from 'better-auth/plugins';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
-		provider: 'pg', // or "mysql", "sqlite"
+		provider: 'pg',
 	}),
 	emailAndPassword: {
 		enabled: true,
