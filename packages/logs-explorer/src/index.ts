1fca09a624334cde8673fa0a08c19d982a397e84
Giordano Ricci <me@giordanoricci.com>
2026-05-09 23:19:27 +0100

    feat(logs-explorer): add react-query option factories

--- packages/logs-explorer/src/index.ts
+++ packages/logs-explorer/src/index.ts
@@ -1,2 +1,8 @@
 export * from "./schemas";
 export * from "./time-range";
+export type { SqlClient } from "./data/client";
+export {
+  LogsRepository,
+  type LogsRepositoryLike,
+} from "./data/repository";
+export * from "./data/options";
